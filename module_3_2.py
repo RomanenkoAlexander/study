557ee783f056cb30f3da4fb580d200979e8cbe5a
RomanenkoAlexander <aleksandr.romanenco@gmail.com>
2024-08-23 10:57:14 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,16 +1,15 @@
 def send_email(message, recipient, *, sender = "university.help@gmail.com"):
     domains = ('.com', '.ru', '.net')
-    if recipient == sender:
+    if not recipient.endswith(domains) or not sender.endswith(domains):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif recipient.find('@') == -1 or sender.find('@') == -1:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
+    elif sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
     else:
-        if recipient.endswith(domains) or sender.endswith(domains):
-            if sender == "university.help@gmail.com":
-                print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
-            else:
-                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
-        else:
-            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
